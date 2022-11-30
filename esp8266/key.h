a15b8a53ec14eda8c7c419ab4a71c79a47affc1e
DONFN <kiagias@protonmail.com>
2022-11-30 12:56:54 +0200

    Fixed firebase auth

--- esp8266/key.h
+++ esp8266/key.h
@@ -1,8 +1,23 @@
-struct Key{
-  String sub; 
-  String id;
-  String sn;
-  int iat;
-  int nbf;
-  int exp;
-};
+// #ifndef Key_h
+// #define Key_h
+
+
+// class Key {
+//   private:
+//     String user; 
+//     String id;
+//     String serialNumber;
+//     long issuedAt;
+//     long notBefore;
+//     long expiresAt;
+
+//   public:
+//     Key(String _id, String _serialNumber, String _user, long _issuedAt, long _notBefore, long _expiresAt);
+//     bool checkSN(String serialNumberFromCard);
+//     bool isExpired();
+//     bool isActivated();
+//     bool isBlacklisted();
+// };
+
+// #endif
+
