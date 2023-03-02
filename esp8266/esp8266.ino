f7f4939cc4a3765be2c7e148617e89975e7fb297
v66v <v66v@Cerberus.Cerberus01>
2023-03-02 17:07:45 +0200

    small changes

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -16,8 +16,8 @@ void setup(void) {
   Serial.print("Trying to connect to ");
   Serial.println(ssid);
 
-	Wifi wifi(ssid, password, NTPServer);
-	Nfc nfc();
+  Wifi wifi(ssid, password, NTPServer);
+  Nfc nfc();
 
   pinMode(D0, OUTPUT);
   pinMode(SPEAKER, OUTPUT);
@@ -39,29 +39,29 @@ void loop(void) {
 
   if(forceOpen) forceOpenTheDoor();
   else if(nfc.readCard()){
-     digitalWrite(BUSY_LED, HIGH);
+    digitalWrite(BUSY_LED, HIGH);
     // Successfuly read the card, proceeding to parse/validate the JWT token.
-		 if(key.setFields(nfc.getCard(),nfc.getSnFromTag()){
-      // Successfuly decoded the JWT token, proceeding to determine if the door can open with the given key.
-      if(allowedToEnter()){
+    if(key.setFields(nfc.getCard(),nfc.getSnFromTag()){
+	// Successfuly decoded the JWT token, proceeding to determine if the door can open with the given key.
+	if(allowedToEnter()){
           // The door can open.
           openTheDoor();
           logEntry();
+	}
+	else{
+	  // Door opening was blocked due to the rules specified in allowedToEnter()
+	  denyEntry();
+	  logUnauthorizedAttempt();
+	}
       }
       else{
-        // Door opening was blocked due to the rules specified in allowedToEnter()
-        denyEntry();
-        logUnauthorizedAttempt();
+	// The card was read successfully, but the token could not be authenticated. That probably means that the card did not have a JWT token written on it
+	// or that the JWT token has been forged.
+	denyEntry();
+	Serial.println("Failed to decode JWT.");
       }
-    }
-    else{
-      // The card was read successfully, but the token could not be authenticated. That probably means that the card did not have a JWT token written on it
-      // or that the JWT token has been forged.
-      denyEntry();
-      Serial.println("Failed to decode JWT.");
-    }
-  }
+      }
 
-  digitalWrite(IDLE_LED, HIGH);
-  delay(150);
-}
+    digitalWrite(IDLE_LED, HIGH);
+    delay(150);
+  }
